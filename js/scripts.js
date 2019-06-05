f002306d14959f1836433c786f3a23912f057ad2
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2019-06-05 11:39:13 -0700

    add radio buttons to streamline calculations

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,17 @@
 var add = function(number1, number2) {
   return number1 + number2;
 };
-
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
-
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    console.log("1st number: " + number1);  // for debugging
+    console.log("2nd number: " + number2);  // for debugging
+    console.log("operator: " + operator);  // for debugging
     var result = add(number1, number2);
     $("#output").text(result);
   });
 });
+
